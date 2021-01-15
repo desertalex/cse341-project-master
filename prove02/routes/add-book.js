dc38fba8f42f78df055c84bf089e4320ae4b1c76
desertalex <alexbraddock22@gmail.com>
2021-01-15 12:39:17 -0700

    Author input section in add books, ui changes

--- prove02/routes/add-book.js
+++ prove02/routes/add-book.js
@@ -15,31 +15,35 @@ router.get('/add-book', (req, res, next) => {
 });
 
 router.post('/add-book', (req, res, next) => {
-    var json = '';
-    fs.readFile('prove02/database.json', (err, data) => {
-        if (data.length == 0) {
-            json = {
-                "books": [
-                    {
-                        "book_title": req.body.book_title,
-                        "book_description": req.body.book_description
-                    }
-                ]
+    if (req.body.book_title != '') {
+        var json = '';
+        fs.readFile('prove02/database.json', (err, data) => {
+            if (data.length == 0) {
+                json = {
+                    "books": [
+                        {
+                            "book_title": req.body.book_title,
+                            "book_author": req.body.book_author,
+                            "book_description": req.body.book_description
+                        }
+                    ]
+                }
+            } else {
+                json = JSON.parse(data);
+                json.books.push({
+                    "book_title": req.body.book_title,
+                    "book_author": req.body.book_author,
+                    "book_description": req.body.book_description
+                });
             }
-        } else {
-            json = JSON.parse(data);
-            json.books.push({
-                "book_title": req.body.book_title,
-                "book_description": req.body.book_description
+            fs.writeFile('prove02/database.json', JSON.stringify(json), (err) => {
+                if (err) {
+                    console.error(err);
+                    throw err;
+                }
             });
-        }
-        fs.writeFile('prove02/database.json', JSON.stringify(json), (err) => {
-            if (err) {
-                console.error(err);
-                throw err;
-            }
-        });
-    })
+        })
+    }
     res.redirect('/prove02/add-book');
 })
 
