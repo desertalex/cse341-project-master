feb97298d27f28ba24a8eb2a8d55fb4708a7b4d2
desertalex <alexbraddock22@gmail.com>
2021-01-29 23:59:15 -0700

    mongoose added

--- index.js
+++ index.js
@@ -13,10 +13,28 @@
 // Our initial setup (package requires, port number setup)
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongodb');
 const path = require('path');
+const cors = require('cors')
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
+
+const corsOptions = {
+    origin: "https://frozen-badlands-14213.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://alexbraddock:4tFVCLKOzVTyWJBT@cluster0.atywg.mongodb.net/shop?retryWrites=true&w=majority";
 
 // Route setup. You can implement more in the future!
 const ta01Routes = require('./routes/ta01');
@@ -51,4 +69,16 @@ app.use(express.static(path.join(__dirname, 'public')))
         // 404 page
         res.render('pages/404', { title: '404 - Page Not Found', path: req.url })
     })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+mongoose
+    .connect(
+        MONGODB_URL, options
+    )
+    .then(result => {
+        // This should be your user handling code implement following the course videos
+        app.listen(PORT);
+    })
+    .catch(err => {
+        console.log(err);
+    });
